0773fd3ff7c213fe0267a76b9cd843af4c892812
Chiara <chiarafdono@gmail.com>
2024-11-30 11:25:03 -0300

    Ya hay dropdowns y botón para iniciar el juego, aún no estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import Board from './components/Board';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+const cardImages = [{src: "/1.jpg", matched: false}, {src: "/2.jpg", matched: false}, 
+  {src: "/3.jpg", matched: false}, {src: "/4.jpg", matched: false}, 
+  {src: "/5.jpg", matched: false}, {src: "/6.jpg", matched: false},
+  {src: "/7.jpg", matched: false}, {src: "/8.jpg", matched: false}, 
+  {src: "/9.jpg", matched: false}, {src: "/10.jpg", matched: false}, 
+  {src: "/11.jpg", matched: false}, {src: "/12.jpg", matched: false}, 
+  {src: "/13.jpg", matched: false}, {src: "/14.jpg", matched: false},
+  {src: "/15.jpg", matched: false}, {src: "/16.jpg", matched: false},
+  {src: "/17.jpg", matched: false}, {src: "/18.jpg", matched: false},
+  {src: "/19.jpg", matched: false}, {src: "/20.jpg", matched: false},
+  {src: "/21.jpg", matched: false}, {src: "/22.jpg", matched: false},
+  {src: "/23.jpg", matched: false}, {src: "/24.jpg", matched: false},
+  {src: "/25.jpg", matched: false}, {src: "/26.jpg", matched: false},
+  {src: "/27.jpg", matched: false}, {src: "/28.jpg", matched: false},
+  {src: "/29.jpg", matched: false}, {src: "/30.jpg", matched: false},
+  {src: "/31.jpg", matched: false}, {src: "/32.jpg", matched: false}];
 
 function App() {
+  const [size, setSize] = useState(0);
+  const [players, setPlayers] = useState(0);
+  const [startGame, setStartGame] = useState(false);
+  const [cards, setCards] = useState([]);
+
+  const shuffleCards = (size) => {
+    const images = cardImages.slice(0, size/2);
+    const pairs = images.concat(images)
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id: Math.random()}));
+    setCards(pairs);
+  };
+
+  const playerOptions = [ 1, 2 ];
+
+  const sizeOptions = [ {value: 16, label:'4x4'}, {value: 36, label:'6x6'}, {value: 36, label: '8x8'}];
+
+  const handlePlayersChange = (option) => {
+    setPlayers(option.value);
+  }
+
+  const handleSizeChange = (option) => {
+    setSize(option.value);
+  }
+
+  const handleClick = () => {
+    shuffleCards(size);
+    setStartGame(true);
+  }
 
   return (
     <div>
-      <Board size={64}/>
-    </div>
+      {startGame ? (
+        <Board size={size} players={players} cards={cards} />
+      ) : (
+      <div>
+        <h1>Memotest</h1>
+        <div>
+          <Dropdown 
+            options={playerOptions} 
+            value={1}
+            onChange={handlePlayersChange}
+            placeholder={"Select the number of players"}
+          /> 
+        </div>
+        <div>
+          <Dropdown 
+            options={sizeOptions} 
+            value={'4x4'}
+            onChange={handleSizeChange}
+            placeholder={"Select the size of the board"}
+          />  
+        </div>
+        <button onClick={handleClick}>New game</button>
+      </div>
+      )}
+      </div>
   )
 }
 
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,25 +3,7 @@ import Card from "./Card";
 import { useEffect, useState } from 'react';
 import './Board.css';
 
-const cardImages = [{src: "/1.jpg", matched: false}, {src: "/2.jpg", matched: false}, 
-    {src: "/3.jpg", matched: false}, {src: "/4.jpg", matched: false}, 
-    {src: "/5.jpg", matched: false}, {src: "/6.jpg", matched: false},
-    {src: "/7.jpg", matched: false}, {src: "/8.jpg", matched: false}, 
-    {src: "/9.jpg", matched: false}, {src: "/10.jpg", matched: false}, 
-    {src: "/11.jpg", matched: false}, {src: "/12.jpg", matched: false}, 
-    {src: "/13.jpg", matched: false}, {src: "/14.jpg", matched: false},
-    {src: "/15.jpg", matched: false}, {src: "/16.jpg", matched: false},
-    {src: "/17.jpg", matched: false}, {src: "/18.jpg", matched: false},
-    {src: "/19.jpg", matched: false}, {src: "/20.jpg", matched: false},
-    {src: "/21.jpg", matched: false}, {src: "/22.jpg", matched: false},
-    {src: "/23.jpg", matched: false}, {src: "/24.jpg", matched: false},
-    {src: "/25.jpg", matched: false}, {src: "/26.jpg", matched: false},
-    {src: "/27.jpg", matched: false}, {src: "/28.jpg", matched: false},
-    {src: "/29.jpg", matched: false}, {src: "/30.jpg", matched: false},
-    {src: "/31.jpg", matched: false}, {src: "/32.jpg", matched: false}];
-
-const Board = ({size}) => {
-    const [cards, setCards] = useState([]);
+const Board = ({size, players, cards}) => {
     const [firstChoice, setFirstChoice] = useState(null);
     const [secondChoice, setSecondChoice] = useState(null);
     const [disabled, setDisabled] = useState(false);
@@ -59,20 +41,11 @@ const Board = ({size}) => {
       setSecondChoice(null);
       setDisabled(false);
     };
-
-    const shuffleCards = (size) => { //si lo dejo así los pares siempre salen en el mismo lugar
-        const shuffledImages = cardImages.sort(() => Math.random() - 0.5);
-        const images = shuffledImages.slice(0, size/2);
-        const pairs = images.concat(images)
-            .map((card) => ({...card, id: Math.random()}));
-        setCards(pairs);
-    };
-
+    
     return (
       <div>
-        <button onClick={() => shuffleCards(size)}>New game</button>
           <div className='board-grid-container'>
-            <div className={'board-grid-64'}>
+            <div className='board-grid-18'>
                 {cards.map(card => (
                     <Card 
                       key={card.id} 
